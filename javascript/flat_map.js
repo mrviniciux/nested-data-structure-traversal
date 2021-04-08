664deb40eee30da82a6476fd9410120d2f923640
Marcos Santos <marcos@intellimize.com>
2021-04-08 09:15:11 -0300

    using flatmap function

---
+++ javascript/flat_map.js
@@ -0,0 +1,48 @@
+const sections = [
+    {
+        "title": "Getting started",
+        "reset_lesson_position": false,
+        "lessons": [
+            {"name": "Welcome"},
+            {"name": "Installation"}
+        ]
+    },
+
+    {
+        "title": "Basic operator",
+        "reset_lesson_position": false,
+        "lessons": [
+            {"name": "Addition / Subtraction"},
+            {"name": "Multiplication / Division"}
+        ]
+    },
+
+    {
+        "title": "Advanced topics",
+        "reset_lesson_position": true,
+        "lessons": [
+            {"name": "Mutability"},
+            {"name": "Immutability"}
+        ]
+    }
+];
+
+const lessonPos = (section, idxSection, idxLesson) => {
+    if(!section['reset_lesson_position'] && idxSection !== 1){
+        return (idxLesson+idxSection) + 1;
+    }
+
+    return idxLesson + 1;
+}
+
+const flattedSections = sections.flatMap((section, idxSection) => {
+    return [{
+        ...section,
+        ...{position: (idxSection+1)},
+        ...{lessons: section.lessons.flatMap((lesson, idxLesson) => {
+                return [{...lesson, ...{position: lessonPos(section, (idxSection+1), idxLesson) } }] }  )
+        }
+    }]
+});
+
+console.log(JSON.stringify(flattedSections, null, 2))
